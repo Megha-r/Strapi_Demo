8c26cb392857c92659d545d1ef741c286bc46395
Megha-r <megha.r@successivesoftwares.com>
2019-02-06 18:57:27 +0530

    dynamic collection name

--- src/lib/ContentProvider.js
+++ src/lib/ContentProvider.js
@@ -24,7 +24,7 @@ class ContentProvider extends React.Component {
       const er = 'Specify the correct route and language key'
       this.setState({ cmsError: er, loading: false })
     } else {
-      const response = await axios(`http://localhost:1337/${collectionName}?routeCode=${routeCode}`)
+      const response = await axios(`http://localhost:1337/${collectionName}s?routeCode=${routeCode}`)
       let data = response.data
       const obj = {}
       data.forEach(element => {
