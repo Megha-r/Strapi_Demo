a0e4a54c1fb0e01577f55bc33b8d7a8c8b7da70d
Megha-r <megha.r@successivesoftwares.com>
2019-02-07 13:45:39 +0530

    public folder deleted

--- src/lib/ContentProvider.js
+++ src/lib/ContentProvider.js
@@ -14,25 +14,28 @@ class ContentProvider extends React.Component {
     }
   }
 
-//*********  Function to fetch data from collection on the basis of routeCode **************** 
-  
+  //*********  Function to fetch data from collection on the basis of routeCode **************** 
+
 
   getdata = async (data) => {
-    const { language,collectionName, routeCode } = data
+    let { language, collectionName, routeCode } = data
     this.setState({ loading: true })
-    if ((language && language === '') || (routeCode && routeCode === '') ||  (collectionName && collectionName === '')) {
-      const er = 'Specify the correct route and language key'
+    language = language.trim();
+    collectionName = collectionName.trim();
+    routeCode = routeCode.trim();
+    if (!(language && routeCode && collectionName)) {
+      const er = 'Specify the correct route, language and collection key'
       this.setState({ cmsError: er, loading: false })
     } else {
-      //`http://localhost:1337?collectionName=${collectionName}?routeCode=${routeCode}`
-      const response = await axios(`http://localhost:1337/${collectionName}?routeCode=${routeCode}`)
+      const response = await axios(`http://localhost:1337/${collectionName}?routeCode=${routeCode}`);
       let data = response.data
       const obj = {}
-      data.forEach(element => {
+      data && data.length && data.forEach(element => {
         obj[element.code] = element[language]
       })
       this.setState({ cmsContent: obj, loading: false })
     }
+
   }
 
   render() {
